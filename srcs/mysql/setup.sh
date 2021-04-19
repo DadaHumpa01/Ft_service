bc0abc87bf11e47f5514c9e57997c270806a7832
Danilo Brignone <dbrignon@e3r8p7.42roma.it>
2021-04-19 14:33:34 +0200

    prima volta che wordpress funziona

--- srcs/mysql/setup.sh
+++ srcs/mysql/setup.sh
@@ -1,16 +1,4 @@
 #!/bin/sh
 
-openrc default
-openrc boot
-
-/etc/init.d/mariadb setup
-
-echo "create database wordpress;" | mysql
-echo "grant all on *.* to 'admin'@'%' identified by 'password' with grant option; flush privileges;" | mysql
-
-rc-service mariadb start
-
-mysql wpdb < wordpress.sql
-
-rc-service mariadb stop
-/usr/bin/mysqld_safe
+mysql_install_db --user=root --datadir=/var/lib/mysql
+/usr/bin/mysqld --user=root --init_file=/init_file
